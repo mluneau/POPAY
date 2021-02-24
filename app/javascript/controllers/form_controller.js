1d952d0b99200cb596f53d26817e6a8de0360989
Pierre Olive <pierre.olive4@gmail.com>
2021-02-24 16:59:35 +0100

    nav ok

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -1,8 +1,28 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = ['amount', 'dueDate', 'submit']
+  static targets = ['amount', 'date', 'submit', 'sliderAmount', 'displayAmount', 'selectDate', 'dateValue', 'spanAmount']
   connect() {
-    console.log('Hello!');
+    console.log(this.selectDateTarget)
+  }
+
+  goToDate() {
+    this.amountTarget.classList.toggle('d-none')
+    this.dateTarget.classList.toggle('d-none')
+  }
+
+  goToSubmit() {
+    this.dateTarget.classList.toggle('d-none')
+    this.submitTarget.classList.toggle('d-none')
+  }
+
+  updateAmount() {
+    this.displayAmountTarget.innerText = this.sliderAmountTarget.value
+    this.spanAmountTarget.innerText = this.sliderAmountTarget.value
+  }
+
+  updateDate() {
+
+    // this.dateValueTarget.innerText = this.selectDateTarget.value
   }
 }
