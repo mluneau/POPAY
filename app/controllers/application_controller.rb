67a76572100a6baa4125c8a261ce496cb066f3a6
Pierre Olive <pierre.olive4@gmail.com>
2021-02-24 14:11:48 +0100

    install active storage and add new avatar input

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:company_id])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:company_id, :avatar])
 
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
