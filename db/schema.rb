f2ebde4278627b08c7ecea9845e788662bbc8aa1
Pierre Olive <pierre.olive4@gmail.com>
2021-02-25 16:19:56 +0100

    transcaction user journey done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_24_130632) do
+ActiveRecord::Schema.define(version: 2021_02_25_150819) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -119,6 +119,7 @@ ActiveRecord::Schema.define(version: 2021_02_24_130632) do
     t.bigint "bank_account_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "comment"
     t.index ["bank_account_id"], name: "index_transactions_on_bank_account_id"
     t.index ["user_id"], name: "index_transactions_on_user_id"
   end
