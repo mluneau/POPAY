1e951e3b51b90716da3f9f9da65d875f9a641d50
Louis A <lajacques@hotmail.fr>
2021-02-25 12:31:42 +0100

    add column time to articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_25_111410) do
+ActiveRecord::Schema.define(version: 2021_02_25_112809) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2021_02_25_111410) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "time"
   end
 
   create_table "bank_accounts", force: :cascade do |t|
