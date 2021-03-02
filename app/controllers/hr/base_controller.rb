ac2f955ffacd2133e677f676d22ac130779a9e08
Pierre Olive <pierre.olive4@gmail.com>
2021-03-02 14:06:02 +0100

    block

--- app/controllers/hr/base_controller.rb
+++ app/controllers/hr/base_controller.rb
@@ -2,7 +2,6 @@ class Hr::BaseController < ApplicationController
   before_action :verify_position
 
   private
-
   def verify_position
     redirect_to root_path if current_user&.position != "hr"
     flash.alert = "Vous n'êtes pas autorisé à utiliser ces fonctionnalités"
