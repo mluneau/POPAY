3cd1ed63fb66c899795197a610b4e5ca006cf7f2
Pierre Olive <pierre.olive4@gmail.com>
2021-03-02 12:46:23 +0100

    avatar ang login page update

--- app/controllers/hr/transactions_controller.rb
+++ app/controllers/hr/transactions_controller.rb
@@ -1,5 +1,5 @@
 class Hr::TransactionsController < ApplicationController
   def index
-    @transactions = policy_scope(Transaction)
+    @transactions = policy_scope(Transaction).order(due_date: :desc)
   end
 end
