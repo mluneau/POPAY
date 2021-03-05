627f747b6ed353f18dc946ecac6eb8ff88259a20
Pierre Olive <pierre.olive4@gmail.com>
2021-03-05 10:49:29 +0100

    remove on off button on notif tab

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,7 @@
 class NotificationsController < ApplicationController
 
   def index
+    @disable_header_mobile = true
     @notifications = policy_scope(Notification).where(user: current_user)
     @disable_container = @current_user.position == "hr"
   end
